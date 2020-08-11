7eded5716e7074a9482c8c1a137ad5a4217f15e3
Admin <Admin@Admin-PC>
2020-08-11 18:50:32 +0530

    First Commit

---
+++ ERP/src/main/java/utility/CommonData.java
@@ -0,0 +1,11 @@
+package utility;
+
+public class CommonData {
+
+
+public static final String URL = "https://www.qabible.in/erp/login";
+public static final String filePath = "C:\\Users\\Admin\\eclipse-workspace\\ERP\\src\\main\\java\\testData\\exceldata.xlsx";
+public static final String testData = "exceldata.xlsx";
+
+}
+
